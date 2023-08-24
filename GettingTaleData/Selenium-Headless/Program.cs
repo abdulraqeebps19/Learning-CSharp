de9d39183fb3ae6b1103b07f9b65076184641508
Abdul Raqeeb <abdul.raqeeb@Powersoft19.com>
2023-08-25 00:08:24 +0500

    headless chrome is working and a try to scrap data with HtmlAgilityPack

---
+++ GettingTaleData/Selenium-Headless/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+class Program
+{
+    static void Main()
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--headless");
+        options.AddArgument("--disable-gpu");
+
+        using (IWebDriver driver = new ChromeDriver(options))
+        {
+            string url = "https://learn.microsoft.com/en-us/troubleshoot/sql/releases/download-and-install-latest-updates#latest-updates-available-for-currently-supported-versions-of-sql-server";
+            driver.Navigate().GoToUrl(url);
+
+            IWebElement table = driver.FindElement(By.XPath("//table[@aria-label='Table 1']"));
+            var tableRows = table.FindElements(By.TagName("tr"));
+            String strRowData = "";
+
+            // Traverse each row
+            foreach (var elemTr in tableRows)
+            {
+                // Fetch the columns from a particuler row
+                List<IWebElement> lstTdElem = new List<IWebElement>(elemTr.FindElements(By.TagName("td")));
+                if (lstTdElem.Count > 0)
+                {
+                    // Traverse each column
+                    foreach (var elemTd in lstTdElem)
+                    {
+                        // "\t\t" is used for Tab Space between two Text
+                        strRowData = strRowData + elemTd.Text + "\t\t";
+                    }
+                }
+                else
+                {
+                    // To print the data into the console
+                    Console.WriteLine("This is Header Row");
+                    Console.WriteLine(tableRows[0].Text.Replace(" ", "\t\t"));
+                }
+                Console.WriteLine(strRowData);
+                strRowData = String.Empty;
+            }
+            Console.WriteLine("");
+            driver.Quit();
+        }
+    }
+}
---
+++ GettingTaleData/WebScrapping/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://learn.microsoft.com/en-us/troubleshoot/sql/releases/download-and-install-latest-updates#latest-updates-available-for-currently-supported-versions-of-sql-server";
+
+        HtmlWeb web = new HtmlWeb();
+        HtmlDocument doc = web.Load(url);
+
+        var tableRows = doc.DocumentNode.SelectNodes("//table[@aria-label='Table 1']//tr");
+
+        if (tableRows != null)
+        {
+            foreach (HtmlNode row in tableRows)
+            {
+                HtmlNodeCollection cells = row.SelectNodes("td");
+                if (cells != null)
+                {
+                    foreach (HtmlNode cell in cells)
+                    {
+                        Console.WriteLine(cell.InnerText.Trim());
+                    }
+                }
+            }
+        }
+    }
+}
